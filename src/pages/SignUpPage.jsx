641d7624a9dff6848b12b213c27f76268beebd02
Adam Yee <adamyee96@gmail.com>
2024-07-05 18:52:51 +0800

    feat: add signup page, api user data to backend

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,27 +1,83 @@
+import { useState } from "react";
+import myImg from "../assets/jiaksimi1.png";
+import { hashData } from "../../util/security";
+import { signUp } from "../../service/users";
 import {
   Card,
   CardHeader,
   CardBody,
-  CardFooter,
   Button,
-  Text,
   Input,
   InputRightElement,
   InputGroup,
-  Box,
   Image,
+  Container,
+  FormControl,
 } from "@chakra-ui/react";
-import { useState, setShow } from "react";
-import { Link } from "react-router-dom";
-import myImg from "../assets/jiaksimi1.png";
 
 export default function SignUpPage() {
   const [show, setShow] = useState(false);
-  const handleClick = () => setShow(!show);
+  const handleShow = () => setShow(!show);
+
+  const [formState, setFormState] = useState({});
+  const [disable, setDisable] = useState(true);
+
+  function handleChange(evt) {
+    setFormState((prevState) => ({
+      ...prevState,
+      [evt.target.name]: evt.target.value,
+    }));
+    setDisable(checkPassword());
+  }
+
+  function checkPassword() {
+    var currForm = formState;
+    if (!currForm.password) {
+      return true;
+    }
+    if (!currForm.confirm) {
+      return true;
+    }
+    if (currForm.password !== currForm.confirm) {
+      return true;
+    }
+    if (currForm.password.length < 8) {
+      return true;
+    }
+    return false;
+  }
+
+  function hashPassword() {
+    var currForm = formState;
+    if (currForm.password) {
+      console.log(currForm.password);
+      var hash = hashData(currForm.password);
+      currForm.password = hash.hash;
+      currForm.salt = hash.salt;
+      currForm.iterations = hash.iterations;
+    }
+  }
+
+  async function handleSubmit(evt) {
+    try {
+      evt.preventDefault();
+
+      hashPassword();
+      const formData = { ...formState };
+      delete formData.error;
+      delete formData.confirm;
+
+      console.log(formData);
+      const user = await signUp(formData);
+      console.log(user);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   return (
     <main>
-      <Box mt="24">
+      <Container centerContent={true}>
         <Card align="center">
           <CardHeader>
             <Image
@@ -33,28 +89,42 @@ export default function SignUpPage() {
           </CardHeader>
 
           <CardBody>
-            <Input type="text" placeholder="Username" />
-            <InputGroup size="md" mt="4">
+            <FormControl as="form" onSubmit={handleSubmit}>
               <Input
-                pr="4.5rem"
-                type={show ? "text" : "password"}
-                placeholder="Enter password"
+                name="username"
+                id="username"
+                placeholder="Username"
+                type="text"
+                onChange={handleChange}
               />
-              <InputRightElement width="4.5rem">
-                <Button h="1.75rem" size="sm" onClick={handleClick}>
-                  {show ? "Hide" : "Show"}
-                </Button>
-              </InputRightElement>
-            </InputGroup>
+              <InputGroup size="md" mt="4">
+                <Input
+                  name="password"
+                  id="password"
+                  pr="4.5rem"
+                  type={show ? "text" : "password"}
+                  placeholder="Enter password"
+                  onChange={handleChange}
+                />
+                <InputRightElement width="4.5rem">
+                  <Button h="1.75rem" size="sm" onClick={handleShow}>
+                    {show ? "Hide" : "Show"}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              <Button
+                mt="8"
+                w="full"
+                colorScheme="blue"
+                disabled={disable}
+                type="submit"
+              >
+                Create Account
+              </Button>
+            </FormControl>
           </CardBody>
-          <CardFooter>
-            {/* TODO: Need to handle submit */}
-            <Button w="full" colorScheme="blue">
-              Create Account
-            </Button>
-          </CardFooter>
         </Card>
-      </Box>
+      </Container>
     </main>
   );
 }
