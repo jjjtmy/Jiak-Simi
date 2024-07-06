f5ee05fac314cc32c05f60c5afe135ee552fc3f6
Adam Yee <adamyee96@gmail.com>
2024-07-06 19:25:17 +0800

    fix(api): add signup to baseurl

--- api/users.js
+++ api/users.js
@@ -2,5 +2,5 @@ import sendRequest from "../util/send-request";
 const BASE_URL = "http://localhost:3000/users";
 
 export async function signUp(userData) {
-  sendRequest(BASE_URL, "POST", userData);
+  sendRequest(`${BASE_URL}/signup`, "POST", userData);
 }
