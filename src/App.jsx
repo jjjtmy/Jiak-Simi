502620d8c40732a3d19b01fa88772216955765a3
Adam Yee <adamyee96@gmail.com>
2024-07-15 15:12:47 +0800

    feat: add redirect for login and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { ChakraProvider, Button } from "@chakra-ui/react";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import AddReviewPage from "./pages/AddReviewPage";
 import ProfilePage from "./pages/ProfilePage";
 import HomePage from "./pages/HomePage";
+import { getUser } from "../service/users";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState(getUser)
 
+  useEffect(() => {
+    if (user) {
+      setUser(getUser());
+    }
+  }, [user]);
+
+  
   return (
     <ChakraProvider>
       <main className="App">
@@ -19,18 +28,18 @@ function App() {
         </Button>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={user ? <Navigate to='/' /> : <LoginPage setUser={setUser} user={user}/>} />
+          <Route path="/signup" element={user ? <Navigate to='/' /> : <SignUpPage setUser={setUser} user={user}/>} />
           <Route
             path="/myprofile"
             element={
-              loggedIn ? <ProfilePage /> : <Navigate to="/login" />
+              user ? <ProfilePage /> : <Navigate to="/login" replace={true} />
             }
           />
           <Route
             path="/addmakan"
             element={
-              loggedIn ? <AddReviewPage /> : <Navigate to="/login" />
+              user ? <AddReviewPage /> : <Navigate to="/login" replace={true}/>
             }
           />
         </Routes>
