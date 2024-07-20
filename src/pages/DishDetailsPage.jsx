db94a18db6c0d9e8d1595aa5a42ca5ca5e04a9f3
ehdahm <45594732+ehdahm@users.noreply.github.com>
2024-07-20 09:24:17 +0800

    fix: username display issue (#42)

--- src/pages/DishDetailsPage.jsx
+++ src/pages/DishDetailsPage.jsx
@@ -38,9 +38,8 @@ export default function DishDetailsPage() {
       
       const reviewsWithUsernames = await Promise.all(
         dishReviews.map(async (review) => {
-          const userObj = await getUsername(review.user_id);
-          const username = userObj.username
-          console.log('DD username', username)
+          const username = await getUsername(review.user_id);
+          console.log('username in DD', username)
           return { ...review, username };
         })
       )
